0f22470006bce06d0040cf6963af8969b0501224
Rajat <113114199+rajatmaharathi@users.noreply.github.com>
2024-03-13 11:29:18 +0530

    intial commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import PageNav from "../components/PageNav";
+
+export default function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+    </div>
+  );
+}
