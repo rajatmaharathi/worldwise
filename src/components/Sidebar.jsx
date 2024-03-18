a05ff4d1ece4c0bc98921d44124ab8a426a74bd2
Rajat <113114199+rajatmaharathi@users.noreply.github.com>
2024-03-18 19:00:17 +0530

    City list added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <div className={styles.sidebar} side>
+    <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by Worldwise Inc.
